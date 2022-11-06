b699fa8ff61d23f063b6006e763fcc3bc427b9d7
ragdelaed <ragdelaed@hotmail.com>
2022-11-06 11:54:24 -0500

    first

---
+++ whois_api_server.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import json, subprocess
+from flask import Flask, request, jsonify, make_response
+
+
+app = Flask(__name__)
+
+
+def queryDomain(domain):
+    a = subprocess.run(["/usr/local/bin/pwhois","-j","%s"%domain],capture_output=True).stdout
+    j = json.loads(a)
+    return j
+
+
+@app.route('/query/', methods=['GET'])
+def lookupDomain():
+    domain = request.args.get('domain')
+    x = queryDomain(domain)
+    return make_response(jsonify({'Query': x}))
+
+
+if __name__ == '__main__':
+    app.run(port=9119, debug=True)
+
